78e5733395402efbd1b3bef152e06dd47e8df50b
Alex Barghi <bgamer50@gmail.com>
2013-06-17 09:52:40 -0400

    Improved commenting, added access to the config file which holds directories to back up

--- fileManager.py
+++ fileManager.py
@@ -1,8 +1,16 @@
-#This class is reposible for executing all actions related to managing a list of files (and directories).
+#This script is reposible for executing all actions related to managing a list of files (and directories).
 import shutil
 import os
-homeDirectory = os.path.expanduser(os.path.join('~'))
-directories = []
-directories.add("Documents")
-print(homeDirectory)
 
+#Records the path of the home directory, which includes the config file that says which directories to back up.
+#Hopefully I'll have a nice web interface to handle creating that config file at some point.
+homeDirectory = os.path.expanduser(os.path.join('~'))
+
+directories = []
+directoryFile = open(os.path.expanduser(os.path.join('~')) + "/.backupManager.config")
+
+for line in directoryFile:
+	directories.append(line.rstrip())
+
+print(homeDirectory)
+print(directories)
