50759ac3ef9fac4d1c220acbbf732595a1094b30
Alex Barghi <bgamer50@andrewjackson.home>
2013-06-19 16:41:42 -0400

    Now acts like an indexer, making an (unreadable) XML file indexing the user's Pictures directory.

--- fileManager.py
+++ fileManager.py
@@ -3,23 +3,11 @@ import shutil
 import os
 from bs4 import BeautifulSoup
 
-#Records the path of the home directory, which includes the config file that says which directories to back up.
-#Then accesses the config file and makes a list of which directories will be backed up.
-#Hopefully I'll have a nice web interface to handle creating that config file at some point.
-homeDirectory = os.path.expanduser(os.path.join('~'))
-
-directories = []
-directoryFile = open(homeDirectory + "/.backupManager/directories.list")
-
-for line in directoryFile:
-	directories.append(line.rstrip())
-fileRecords = []
-
-#Build XML
-for dir in directories:
+def computeDirectory(dir, homeDirectory, masterRecord):
 	fileRecord = BeautifulSoup('<directory>', 'xml')
 	fileRecord.directory.attrs['path'] = homeDirectory + "/" + dir
-	
+	masterRecord = fileRecord
+
 	modifiedTag = fileRecord.new_tag('modified')	
 	modifiedTag.string = "01-01-1970 0:00"
 	fileRecord.directory.append(modifiedTag)
@@ -38,13 +26,47 @@ for dir in directories:
 		fileRecord.directory.append(fileTag)
 		
 	for d in directoryOnlyTree:
-		dirTag = fileRecord.new_tag('directory')
-		dirTag.attrs['path'] = fileRecord.directory.attrs['path'] + "/" + d
-		fileRecord.directory.append(dirTag)
+		#dirTag = fileRecord.new_tag('directory')
+		#dirTag.attrs['path'] = fileRecord.directory.attrs['path'] + "/" + d
+		#fileRecord.directory.append(dirTag)
+		dirRecord = computeDirectory(d, homeDirectory + "/" + dir, masterRecord)
+		fileRecord.directory.append(dirRecord)
+	return fileRecord
 
+
+#Records the path of the home directory, which includes the config file that says which directories to back up.
+#Then accesses the config file and makes a list of which directories will be backed up.
+#Hopefully I'll have a nice web interface to handle creating that config file at some point.
+homeDirectory = os.path.expanduser(os.path.join('~'))
+
+directories = []
+directoryFile = open(homeDirectory + "/.backupManager/directories.list")
+
+for line in directoryFile:
+	directories.append(line.rstrip())
+fileRecords = []
+
+#Build XML
+for dir in directories:
+	fileRecord = computeDirectory(dir, homeDirectory, None)
 	fileRecords.append(fileRecord)
 
+file = open("test.xml", 'w')
 for f in fileRecords:
-	print(f.prettify())
+	file.write(f.prettify())
 	print("\n")
+file.close()
 
+file2 = open("test.xml")
+corrected = []
+for line in file2:
+	if "?xml" not in line:
+		corrected.append(line)
+file2.close()
+
+file3 = open("test.xml", 'w')
+file3.write('<?xml version = "1.0" encoding = "utf-8"?>')
+file3.write("\n")
+for line in corrected:
+	file3.write(line)
+file3.write('<?/xml>')
